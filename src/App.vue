fbf30ed7d182d9e00afe1ae3d12592bffe5ba2cd
impnem <impnem@gmail.com>
2022-05-07 21:24:41 +0900

    12강

--- src/App.vue
+++ src/App.vue
@@ -8,56 +8,42 @@
 
       <v-app-bar-nav-icon v-on:click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Page title</v-toolbar-title>
+      <site-title :title="title"></site-title>
 
       <v-spacer></v-spacer>
-
-      <v-btn icon to="/about">
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon to="/">
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
 
     </v-app-bar>
 
     <v-navigation-drawer app v-model="drawer">
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="text-h6">
-            Application
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            subtext
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-      <v-divider></v-divider>
+      <site-menu></site-menu>
     </v-navigation-drawer>
 
     <v-content>
       <router-view></router-view>
     </v-content>
 
-    <v-footer app padless color="primary" dark absolute>
-      <v-col
-        class="text-center"
-        cols="12"
-      >
-        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-      </v-col>
-    </v-footer>
+    <site-footer :footer="footer"></site-footer>
 
   </v-app>
 </template>
 
 <script>
+import SiteTitle from '@/views/site/titleView.vue'
+import SiteFooter from '@/views/site/footerView.vue'
+import SiteMenu from '@/views/site/menuView.vue'
+
 export default {
+  components: {
+    SiteTitle,
+    SiteFooter,
+    SiteMenu
+  },
   name: 'App',
   data () {
     return {
-      drawer: false
+      drawer: false,
+      title: '나의 타이틀입니다',
+      footer: '푸터입니다'
     }
   }
 }
---
+++ src/views/site/footerView.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-footer app color="primary" dark absolute>
+    <v-spacer></v-spacer>
+    <div>&copy; {{ new Date().getFullYear() + ' ' + footer }}</div>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  props: ['footer']
+}
+</script>
+
+<style>
+
+</style>
